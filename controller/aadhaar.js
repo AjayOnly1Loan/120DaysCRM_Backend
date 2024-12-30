1374b4179d8cee98be6e2df1bb74a3db764b255a
Abhay <abhay@only1loan.com>
2024-12-30 16:19:08 +0530

    added slash

--- controller/aadhaar.js
+++ controller/aadhaar.js
@@ -22,8 +22,8 @@ export const generateAadhaarLink = asyncHandler(async (req, res) => {
     console.log(req.session);
 
     const customerName = `${fName}${mName && ` ${mName}`} ${lName}`;
-    const link = `https://api.120daysfinance.com/verify-aadhaar${id}`;
-    // const link = `http://localhost:8081/verify-aadhaar${id}`;
+    const link = `https://api.120daysfinance.com/verify-aadhaar/${id}`;
+    // const link = `http://localhost:8081/verify-aadhaar/${id}`;
     // const result = await aadhaarKyc(lead.mobile, lead.fName, lead.lName, link);
 
     // if (result.data.ErrorMessage === "Success") {
