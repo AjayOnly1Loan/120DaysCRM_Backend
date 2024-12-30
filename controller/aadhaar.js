270288d62127a9a78783659ddb81b9c83b87c863
Abhay <abhay@only1loan.com>
2024-12-30 16:30:47 +0530

    destructure id

--- controller/aadhaar.js
+++ controller/aadhaar.js
@@ -48,7 +48,9 @@ export const generateAadhaarLink = asyncHandler(async (req, res) => {
 // @route POST /api/verify/aadhaar/:id
 // @access Private
 export const aadhaarOtp = asyncHandler(async (req, res) => {
-    const id = req.params;
+    const {id} = req.params;
+
+    console.log('id',id)
 
     const lead = await Lead.findById(id);
     const aadhaar = lead?.aadhaar;
@@ -79,7 +81,7 @@ export const aadhaarOtp = asyncHandler(async (req, res) => {
 // @route PATCH /api/verify/aaadhaar-otp/:id
 // @access Private
 export const saveAadhaarDetails = asyncHandler(async (req, res) => {
-    const id = req.params;
+    const {id} = req.params;
     const { otp, transactionId, fwdp, codeVerifier } = req.body;
 
     // Check if both OTP and request ID are provided
